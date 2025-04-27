d3f0da684f5a858f92159b6adca45c332928a07d
nietiene <niyomugaboetiene53@gmail.com>
2025-04-27 15:50:02 -0700

    MySQL Db

--- Self-Study/MySQL/userRoute.js
+++ Self-Study/MySQL/userRoute.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const mysql = require("mysql2");
 const router = express.Router();
+const session = require("express-session");
+
+// Initialize session middleware
+router.use(session({
+    secret: 'factorise@123', // Your secret key can be random
+    resave: false, // avoid to save every action
+    saveUninitialized: true, // save only empty or new action
+    cookie: {secure: false} // when using hhtp we set false
+}));
 
 const connection = mysql.createConnection({
     host:process.env.HOST_NAME,
@@ -9,6 +18,71 @@ const connection = mysql.createConnection({
     database: process.env.DATABASE_NAME
 });
 
+//Check Authorised User
+router.use((req, res, next) => {
+    res.set('Cache-control', 'no-store');
+    next();
+})
+const Authorised = (req, res, next) => {
+    if (req.session.isLoggedIn) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+// Show login page
+router.get('/login', (req, res) => {
+    res.render("login", {error: null}) // null means allow every user to access login page 
+});
+
+router.post('/login', (req, res) => {
+
+    const {name, place} = req.body; // get credential from the user
+    const sql = "SELECT * FROM self WHERE name = ? AND place = ?";
+    connection.query(sql, [name, place], (err, result) => {
+        if (err) {
+            // throw err;
+            console.log(err);
+            return res.render('login', {error: 'Database Error'});
+        }
+        if (result.length > 0) {
+             req.session.isLoggedIn = true;
+             req.session.name = name;
+             res.redirect("/user");
+        } else {
+            return res.render('login', {error: 'invalid name or place'});
+        }
+    } )
+});
+
+/// Create account
+router.get('/register', (req, res) => {
+    res.render('register', {errors: null});
+});
+
+router.post('/register', (req, res) => {
+    const {name, place} = req.body;
+   const sql = "INSERT INTO self(name, place) VALUES(?, ?)";
+   connection.query(sql, [name, place], (err) => {
+    if (err) {
+        res.render("register", {errors: "Database Error"});
+    } else {
+        res.redirect('/login');
+    }
+   })
+});
+// logout
+
+router.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            throw err;
+        } else {
+            res.redirect('/login');
+          }
+    });
+})
 // database connection
 connection.connect((err) => {
     if (err) {
@@ -18,16 +92,16 @@ connection.connect((err) => {
     }
 });
 
-router.get('/', (req, res) => {
+router.get('/', Authorised,(req, res) => {
     res.send("Welcome to Backend side of app");
 })
-//insert data in database
 
-router.get('/add', (req, res) => {
+//insert data in database
+router.get('/add', Authorised,(req, res) => {
     res.render("insert");
 });
 
-router.post('/add', (req, res) => {
+router.post('/add', Authorised,(req, res) => {
 
     const {name, place} = req.body;
     const sqlQuery = "INSERT INTO self(name, place) VALUES(?, ?)";
@@ -35,6 +109,7 @@ router.post('/add', (req, res) => {
         if (err) {
              throw err;
         } else {
+            req.session.message = "User Added Successfully";
             res.status(200).redirect("/user");
         }
     });
@@ -42,20 +117,22 @@ router.post('/add', (req, res) => {
 
 // Retieve data
 
-router.get('/user', (req, res) => {
+router.get('/user', Authorised,(req, res) => {
 
     const sql =  "SELECT * FROM self";
     connection.query(sql,  (err, data) => {
         if (err) {
             throw err;
         } else {
-            res.status(200).render("user", {user: data});
+            const name = req.session.name;
+            const message = req.session.message;
+            res.status(200).render("user", {user: data, message, name});
         }
     });
 })
 
 // update 
-router.get('/update/:id', (req, res) => {
+router.get('/update/:id', Authorised,(req, res) => {
     const id = parseInt(req.params.id);
     const slqUpdate = "SELECT * FROM self WHERE id = ?";
     connection.query(slqUpdate, id, (err, data) => {
@@ -68,7 +145,7 @@ router.get('/update/:id', (req, res) => {
 
 });
 
-router.post('/update/:id', (req, res) => {
+router.post('/update/:id', Authorised,(req, res) => {
     const id = parseInt(req.params.id);
     const {name, place} = req.body;
     const sqlUpdate = `UPDATE self SET name = ?, place = ? WHERE id = ${id}`;
@@ -76,18 +153,20 @@ router.post('/update/:id', (req, res) => {
         if (err) {
             throw err;
         } else {
+            req.session.message = "User updated successfully";
             res.status(200).redirect('/user');
         }
     })
 })
 // delete 
-router.post('/delete/:id', (req, res) => {
+router.post('/delete/:id', Authorised,(req, res) => {
     const id = parseInt(req.params.id);
     const sqlDelete = `DELETE FROM self WHERE id = ${id}`;
     connection.query(sqlDelete, (err) => {
         if (err) {
             throw err;
         } else {
+             req.session.message = "User deleted successfully";
             res.status(200).redirect('/user');
         }
     });
