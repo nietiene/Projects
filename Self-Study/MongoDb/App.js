a2ee4baebb0ab5b9f11a099775917b3088331e4c
nietiene <niyomugaboetiene53@gmail.com>
2025-04-28 07:46:32 -0700

    Mongo  Db

--- Self-Study/MongoDb/App.js
+++ Self-Study/MongoDb/App.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const userRoute = require("./userRoute");
+const userRoute = require("./userRoute.js");
 const path = require("path");
 require("dotenv").config();
+const session = require("express-session");
 
 const App = express();
 App.use(bodyParser.json());
-App.use(bodyParser.urlencoded({extended: true}));
+App.use(express.urlencoded({extended: true}));
 App.set("view engine", "ejs");
-App.use("/user", userRoute);
 App.set(express.static(path.join(__dirname, "view")));
 
+App.use(session({
+    secret: 'factorise',
+   resave: false,
+   saveUninitialized: true,
+}));
+
+App.use((req, res, next) => {
+    res.locals.successMsg = req.session.successMsg; // enable message to displaye in ejs
+    res.locals.errorMsg = req.session.errorMsg; // also this
+    delete successMsg; // delete message in session after display it
+    delete errorMsg; // also this
+    next(); // allow user to move to the next page
+});
+App.use("/user", userRoute);
+// if user typed login instead of user/login
+App.get('/login', (req, res) => {
+    res.redirect('/user/login');
+})
 const URL = process.env.CONNECTION_STRING;
 mongoose.connect(URL, {
     useNewUrlParser: true,
--- Self-Study/MongoDb/userRoute.js
+++ Self-Study/MongoDb/userRoute.js
@@ -1,12 +1,97 @@
 const express = require("express");
 const user = require("./userSchema.js");
+const session = require("express-session");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+// Make our system used by only authorised user
+
+const Authorized = (req, res, next) => {
+    if (req.session && req.session.isLoggedIn) {
+        next();
+    } else {
+        res.redirect("/login");
+    }
+}
+
+//Display login Page 
+router.get('/login', (req, res) => {
+    res.render('login', {
+        successMsg: req.session.successMsg,
+        errorMsg: req.session.errorMsg,
+    });
+    delete req.session.successMsg;
+    delete req.session.errorMsg;
+});
+
+// handle login Logic
+router.post('/login', async (req, res) => {
+
+    const {id, name, place} = req.body;
+    try {
+        const existUser = await user.findOne({id: parseInt(id), name, place});
+        if (existUser) {
+            req.session.isLoggedIn = true;
+            req.session.name = existUser.name;
+            req.session.user = {id: existUser.id, name: existUser.name, place: existUser.place};
+            req.session.successMsg = "Welcome to our site";
+            res.redirect('/user');
+        } else {
+            req.session.errorMsg = "Invalid credentials";
+            res.redirect("/user/login");
+        }    
+    } catch (err) {
+        req.session.errorMsg = "Server Error";
+        res.redirect("/user/login");
+    }
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            throw err;
+        } else {
+            res.redirect('/user/login')
+        }
+    });
+});
+
+// Avoid user back In system after logout
+router.use((req, res, next) => {
+    res.set('Cache-control', 'no-store')
+    next();
+})
+// Create account
+router.get('/register', (req, res) => {
+     res.render('register');
+});
+
+// Create  Account
+router.post('/register', async (req, res) => {
+    try {
+        const {id, name, place} = req.body; 
+        const exist = await user.findOne({id: id});
+        if (!exist) {
+            const NewUser = await user.create({id: id, name: name, place: place});
+            if (NewUser)
+            req.session.successMsg = "Register done successfully please create an account";
+            res.redirect('/user/login');
+        } else {
+            req.session.errorMsg = "account does not created please try again";
+            res.redirect('/user/register');
+        }
+        } catch (err) {
+            req.session.errorMsg = "Database Error";
+            res.redirect('/user/register');
+        }
+})
+
+router.get('/', Authorized ,async (req, res) => {
     try {
         const userData = await user.find();
         if (userData.length > 0) {
-           res.render("userData", {user: userData});
+            const name = req.session.name;
+           res.render("userData", {user: userData, name});
         } else {
             res.status(404).send("No data in system")
         }
@@ -14,15 +99,15 @@ router.get('/', async (req, res) => {
         res.status(500).send("Internal server error");
     }
 });
-//get data based on specied Id
+// get data based on specied Id
 
-router.get ('/:id', async(req, res) => {
+router.get ('/:id', Authorized ,async(req, res) => {
     try {
     const id = parseInt(req.params.id);
     const exist = await user.findOne({id: id});
     
     if (exist) {
-        res.render("userData", {user: [exist]});
+        res.render("userData", {user: [exist]}); // Make it as an array to avoid an error
     } else {
         res.status(404).send("User not found");
     }
@@ -30,32 +115,32 @@ router.get ('/:id', async(req, res) => {
     res.status(500).send("Internal server error", err);
  }
 });
-//inset
-router.get('/add', async (req, res) => {
-    res.render("insert");
-});
-// insert
-router.post('/add', async (req, res) => {
-      try {
-        const newUser = await user.create({
-            id: parseInt(req.body.id),
-            name: req.body.name,
-            place: req.body.place
-        });
+// //inset
+// router.get('/Add', Authorized ,async (req, res) => {
+//     res.render("insert");
+// });
+// // insert
+// router.post('/Add', Authorized ,async (req, res) => {
+//       try {
+//         const newUser = await user.create({
+//             id: parseInt(req.body.id),
+//             name: req.body.name,
+//             place: req.body.place
+//         });
 
-        if (newUser) {
-            res.status(201).redirect("/user");
+//         if (newUser) {
+//             res.status(201).redirect("/user");
 
-        } else {
-            res.status(500).send("Data not inserted");
-        }
-      } catch (err) {
-        res.status(500).send( "Data not inseted", err);
-      }
-});
+//         } else {
+//             res.status(500).send("Data not inserted");
+//         }
+//       } catch (err) {
+//         res.status(500).send( "Data not inseted");
+//       }
+// });
 
 // Update user
-router.get('/update/:id', async (req, res) => {
+router.get('/update/:id', Authorized ,async (req, res) => {
      const id = parseInt(req.params.id);
      const index = await user.findOne({id: id});
 
@@ -65,7 +150,7 @@ router.get('/update/:id', async (req, res) => {
 });
 
 // Update user
-router.post('/update/:id', async(req, res) => {
+router.post('/update/:id', Authorized ,async(req, res) => {
        try {
         const id = parseInt(req.params.id);
         const Updated = await user.findOneAndUpdate(
@@ -85,7 +170,7 @@ router.post('/update/:id', async(req, res) => {
 })
 
 // Delete user
-router.post('/delete/:id', async(req, res) => {
+router.post('/delete/:id', Authorized ,async(req, res) => {
       try {
         const id = parseInt(req.params.id);
         const deleted = await user.findOneAndDelete({id: id}) 
